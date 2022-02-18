d3872d13086b4393393a6b97d9b5475f965074b4
tushar <tusharshitre123@gmail.com>
2022-02-18 19:25:09 +0530

    [ Tushar shitre] add no duplicate person

---
+++ AddressBook2/AddressBook2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBookImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to address book");
+            CreateAddressBook createAddressBook = new CreateAddressBook();
+            createAddressBook.ReadInput();
+            Console.ReadLine();
+        }
+    }
+}
